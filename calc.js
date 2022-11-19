c10d50f24d87ec69b8989dae48e1cdb9c0970b75
Chetana_Chaudhary <103587504+chetanachaudhary@users.noreply.github.com>
2022-11-19 21:16:21 +0545

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,26 @@
+let screen = document.querySelector('.screen');
+let buttons = document.querySelectorAll('.btn');
+let clear = document.querySelectorAll('.btn-clear');
+let result = document.querySelectorAll('.btn-result');
+
+buttons.forEach(function (button) {
+    button.addEventListener('click', function (e) {
+        let value = e.target.dataset.num;
+        screen.value += value;
+    })
+})
+result.forEach(function (button) {
+    button.addEventListener('click', function (e) {
+        if (screen.value === "") {
+            screen.value = "Input is empty";
+        }else {
+            let answer =eval(screen.value);
+            screen.value =answer;
+        }
+    })
+})
+clear.forEach(function (button) {
+    button.addEventListener('click', function (e) {
+        screen.value = "";
+    })
+})
